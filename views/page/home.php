b4718e47e4a4e1137360ebcf275a6cb54f9e3103
Kevin Colombani <kevin.colombani@etu.univ-amu.fr>
2018-01-28 22:36:08 +0100

    AAAAAH

--- views/page/home.php
+++ views/page/home.php
@@ -1,2 +1,9 @@
 
-    <h1>Welcome</h1>
+<h1><?php echo text('MAIN_TEXT_0') ?></h1>
+<p>
+	<?php echo text('MAIN_TEXT_1');
+		echo text('MAIN_TEXT_2'); 
+		echo text('MAIN_TEXT_3');
+		echo text('MAIN_TEXT_4');
+	?>
+</p>
