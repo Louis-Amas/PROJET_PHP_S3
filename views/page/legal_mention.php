34245dcb8baf41a361d78849035490bdbbbfbc1c
Kevin Colombani <kevin.colombani@etu.univ-amu.fr>
2018-01-28 23:06:21 +0100

    srgfdhvbjnkl,

---
+++ views/page/legal_mention.php
@@ -0,0 +1,12 @@
+<h1><?php echo text('LEGAL_TEXT_0') ?></h1>
+<p>
+	<?php echo text('LEGAL_TEXT_1')	?>
+	<br>
+	<?php echo text('LEGAL_TEXT_2') ?>
+	<br>
+	<?php echo text('LEGAL_TEXT_3') ?>
+	<br>
+	<?php echo text('LEGAL_TEXT_4') ?>
+	<br>
+	<?php echo text('LEGAL_TEXT_5') ?>
+</p>
